fac14b8a54c13c0ef2fba976a765e442d5b30d73
Jose Rodriguez <jlrodriguezlagos@gmail.com>
2024-11-05 09:51:08 -0500

    Updare CORS

--- calculator.php
+++ calculator.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:8100');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
 header('Acess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
